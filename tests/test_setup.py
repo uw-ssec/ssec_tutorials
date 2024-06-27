322c07be2e3ad0741499aa0561aa8612e1e87a60
Landung 'Don' Setiawan <landungs@uw.edu>
2024-06-27 13:55:14 -0700

    test: Make test more compact with param fixtures

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -6,26 +6,26 @@ from tempfile import TemporaryDirectory
 
 TUTORIALS_CACHE_ENV = "SSEC_TUTORIALS_CACHE"
 
-@pytest.fixture(scope="function")
-def cache_dir():
-    with TemporaryDirectory() as tmpdir:
-        cache_dir = str((Path(tmpdir) / "ssec_tutorials").resolve())
-        os.environ[TUTORIALS_CACHE_ENV] = cache_dir
-        yield cache_dir
+@pytest.fixture(scope="function", params=[False, True])
+def cache_dir(request):
+    if request.param:
+        with TemporaryDirectory() as tmpdir:
+            cache_dir = str((Path(tmpdir) / "ssec_tutorials").resolve())
+            os.environ[TUTORIALS_CACHE_ENV] = cache_dir
+    else:
+        cache_dir = str(Path.home() / ".cache" / "ssec_tutorials")
 
-def test_default_cache():
+    yield cache_dir
+
+    if TUTORIALS_CACHE_ENV in os.environ:
+        del os.environ[TUTORIALS_CACHE_ENV]
+    del sys.modules["ssec_tutorials.setup"]
+
+def test_cache_dir(cache_dir):
     """Tests the tutorial cache directory exists"""
     from ssec_tutorials.setup import TUTORIAL_CACHE
-    cache_dir = str(Path.home() / ".cache" / "ssec_tutorials")
-    assert os.environ.get(TUTORIALS_CACHE_ENV) is None
+    if ".cache" in cache_dir:
+        # Ensure that environment variable is not set
+        assert TUTORIALS_CACHE_ENV not in os.environ
     assert TUTORIAL_CACHE.exists() is True
     assert str(TUTORIAL_CACHE) == cache_dir
-    del sys.modules["ssec_tutorials.setup"]
-
-def test_custom_cache_dir(cache_dir):
-    """Tests the tutorial cache directory exists with custom cache directory"""
-    from ssec_tutorials.setup import TUTORIAL_CACHE
-    assert TUTORIAL_CACHE.exists() is True
-    assert str(TUTORIAL_CACHE) == cache_dir
-    del os.environ[TUTORIALS_CACHE_ENV]
-    del sys.modules["ssec_tutorials.setup"]
