1dcc8d8874aac2c237912706c240e2d8204c3c7f
Landung 'Don' Setiawan <landungs@uw.edu>
2024-06-27 13:48:09 -0700

    test: Tweak test for custom dir

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -7,12 +7,11 @@ from tempfile import TemporaryDirectory
 TUTORIALS_CACHE_ENV = "SSEC_TUTORIALS_CACHE"
 
 @pytest.fixture(scope="function")
-def setup_env():
+def cache_dir():
     with TemporaryDirectory() as tmpdir:
         cache_dir = str((Path(tmpdir) / "ssec_tutorials").resolve())
         os.environ[TUTORIALS_CACHE_ENV] = cache_dir
-        from ssec_tutorials.setup import TUTORIAL_CACHE
-        yield TUTORIAL_CACHE, cache_dir
+        yield cache_dir
 
 def test_default_cache():
     """Tests the tutorial cache directory exists"""
@@ -23,9 +22,9 @@ def test_default_cache():
     assert str(TUTORIAL_CACHE) == cache_dir
     del sys.modules["ssec_tutorials.setup"]
 
-def test_custom_cache_dir(setup_env):
-    """Tests the tutorial cache directory exists"""
-    TUTORIAL_CACHE, cache_dir = setup_env
+def test_custom_cache_dir(cache_dir):
+    """Tests the tutorial cache directory exists with custom cache directory"""
+    from ssec_tutorials.setup import TUTORIAL_CACHE
     assert TUTORIAL_CACHE.exists() is True
     assert str(TUTORIAL_CACHE) == cache_dir
     del os.environ[TUTORIALS_CACHE_ENV]
